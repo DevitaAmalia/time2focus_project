8d1b2435b2640401422aa8f0474e1f1180410a54
DevitaAmalia <devitaamalia30@gmail.com>
2025-11-26 11:46:23 +0800

    abstract class untuk timer

---
+++ src/WorkPhase.java
@@ -0,0 +1,15 @@
+public class WorkPhase extends PomodoroPhase {
+
+    public WorkPhase(int durationSeconds) {
+        super(durationSeconds);
+    }
+
+    @Override
+    public String getPhaseName() {
+        return "Work";
+    }
+
+    @Override
+    public void onPhaseFinished(PomodoroController controller) {
+    }
+}
